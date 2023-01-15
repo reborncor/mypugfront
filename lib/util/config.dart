a11f74073226eb1ff6c70268c9a55ce94282e1f4
John ALLOU <allou.john@hotmail.fr>
2023-01-16 00:03:33 +0100

    fetching change for release production

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,11 +1,21 @@
+//
+// String URL = "http://ec2-13-38-90-229.eu-west-3.compute.amazonaws.com";
+// // String URL = "https://d774-2a04-ee41-83-a529-451f-7244-699f-ed01.ngrok.io";
+//
+// String AWS_URL = "https://bucketmypug.s3.eu-west-3.amazonaws.com";
+// String AWS_BUCKETNAME = "bucketmypug";
+// String AWS_ACCESSKEY = "AKIAT7QFB2DP5XDKBBNS";
+// String AWS_SECRETKEY = "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";
 
-String URL = "http://ec2-13-38-90-229.eu-west-3.compute.amazonaws.com";
+
+String URL = "http://ec2-13-38-227-100.eu-west-3.compute.amazonaws.com";
 // String URL = "https://d774-2a04-ee41-83-a529-451f-7244-699f-ed01.ngrok.io";
 
-String AWS_URL = "https://bucketmypug.s3.eu-west-3.amazonaws.com";
-String AWS_BUCKETNAME = "bucketmypug";
-String AWS_ACCESSKEY = "AKIAT7QFB2DP5XDKBBNS";
-String AWS_SECRETKEY = "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";
+String AWS_URL = "https://mypugbucket.s3.eu-west-3.amazonaws.com";
+String AWS_BUCKETNAME = "mypugbucket";
+String AWS_ACCESSKEY = "AKIASU46GDAJ5SYLMIF2";
+String AWS_SECRETKEY = "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";
+
 
 //REAL : http://ec2-13-38-90-229.eu-west-3.compute.amazonaws.com/
 //WITH BLODENAPI.pem
