808d7b60ffb8927b062f094fec8a6d57c80849b4
joumichy <allou.john@hotmail.fr>
2025-02-27 09:32:23 +0100

    change setup

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,21 +1,11 @@
 //INTERN
 //WITH BLODENAPI.pem
-String URL = "http://ec2-user@ec2-52-47-98-118.eu-west-3.compute.amazonaws.com";
-// String URL = "https://7d8f-194-230-158-138.ngrok-free.app";
-String AWS_URL = "https://bucketmypug.s3.eu-west-3.amazonaws.com";
-String AWS_BUCKETNAME = "bucketmypug";
-String AWS_ACCESSKEY = "AKIAT7QFB2DP5XDKBBNS";
-String AWS_SECRETKEY = "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";
+String URL = "";
+String AWS_URL = "";
+String AWS_BUCKETNAME = "";
+String AWS_ACCESSKEY = "";
+String AWS_SECRETKEY = "";
 String STRIPE_KEY = "";
 String NOTIFICATION_TOKEN = "";
 
-// CLIENT
-//WITH keymypug.pem
-// String URL = "http://ec2-13-38-227-100.eu-west-3.compute.amazonaws.com";
-// String AWS_URL = "https://mypugbucket.s3.eu-west-3.amazonaws.d";
-// String AWS_BUCKETNAME = "mypugbucket";
-// String AWS_ACCESSKEY = "AKIASU46GDAJ5SYLMIF2";
-// String AWS_SECRETKEY = "vNZAgctET+15NziU/0lYJwjU2Uw84t1zIE9 4+SJU";
 
-//DEV : http://ec2-13-38-90-229.eu-west-3.compute.amazonaws.com/
-//PROD : http://ec2-13-38-227-100.eu-west-3.compute.amazonaws.com
